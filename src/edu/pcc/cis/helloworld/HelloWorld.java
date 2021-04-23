8dde6646d14ef03e792c5cd5f84428f817ae9fab
Miguel Amaro <69990043+migo-code@users.noreply.github.com>
2021-04-23 16:43:40 -0700

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,10 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Created by Miguel Amaro 04/23/2021
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
