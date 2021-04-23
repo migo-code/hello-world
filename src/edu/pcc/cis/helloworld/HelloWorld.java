436e986305a108875d27f638b18f0b8fd41ce0b9
Miguel Amaro <69990043+migo-code@users.noreply.github.com>
2021-04-23 16:45:11 -0700

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,10 +1,13 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 /**
  * Created by Miguel Amaro 04/23/2021
  */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
